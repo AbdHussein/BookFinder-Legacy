80f5cecef736fca7372dd46b6ae850163f233666
AbdHussein <abdulrahmaanhussein97@gmail.com>
2020-07-20 15:20:35 +0300

    Continuous Deployment configured

--- server/database/index.js
+++ server/database/index.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose"); //inclode mongoose in our page
 
 //connection to mongodb
 const db = mongoose
-  .connect("mongodb://localhost:27017/books-finder", { useNewUrlParser: true })
+  .connect("mongodb://localhost:27017/books-finder", { useNewUrlParser: true,  useUnifiedTopology: true })
   .then(() => {
     console.log("connected successfully");
   })
--- server/index.js
+++ server/index.js
@@ -10,7 +10,7 @@ app.use(cors());
 
 
 /// circleCi  => test each push
-// cont deployment
+// cont deployment (Done)
 
 // validation for info on signup and login. Search for good module
 // handle errors and render to the user
@@ -18,7 +18,7 @@ app.use(cors());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-app.use(express.static(path.resolve(__dirname + "client", "build")));
+app.use(express.static("client/build"));
 
 
 //take the data of the book that i seach about it and put in favorit list
@@ -26,13 +26,10 @@ app.use('/', router)
 
 // app.get('/', (req, res) => {
 //   res.send('works!')
-// })
-
+// });
 
 
 /// TODO:  authentication verrification/ check for token in every req for every user
-
-
 var port = 5000;
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
